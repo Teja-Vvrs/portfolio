b9d4dd6c784fd381df1c11c8ec6337954f856a25
VVRS_TEJA <tejav2527@gamil.com>
2025-02-20 17:41:14 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-200">
+      <h1 class="text-3xl font-bold underline bg-blue-200">
+              Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
