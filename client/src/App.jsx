4e0377a1d82d19f70520e56f6418d36614821f9c
VVRS_TEJA <tejav2527@gamil.com>
2025-02-21 22:19:25 +0530

    "Thrid"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,15 @@ import React from 'react'
 const App = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-200">
-      <h1 class="text-3xl font-bold underline bg-blue-200">
-              Hello world! This is my protfolio website.
+      <h1 class="text-3xl font-bold underline bg-amber-200">
+              Hello world! This is my protfolio website.      
       </h1>
+      <h1 className="text-3xl font-bold underline bg-amber-200">
+        
+        VVRS TEJA Protfolio
+
+      </h1>
+
     </div>
   )
 }
